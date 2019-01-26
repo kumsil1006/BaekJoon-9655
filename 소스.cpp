fac96f65f495aa88479f9a222342021ecde2014e
Soobeen Yoon <39304306+kumsil1006@users.noreply.github.com>
2019-01-26 16:46:49 +0900

    Update 소스.cpp

--- 소스.cpp
+++ 소스.cpp
@@ -4,22 +4,22 @@ int main()
 {
 	int n;
 	cin >> n;
-	bool sang = false;
-	bool chang = false;
+	bool sang = false; // 상근
+	bool chang = false; // 창영
 	while (1) {
 		if (n > 3) {
 			n -= 3;
-		}
+		} // 남은 돌이 3개 이상이면 3개를 가져감
 		else
-			n--;
+			n--; // 3개 이하이면 1개를 가져감
 
 		if (sang == false) {
 			sang = true;
-			chang = false;
+			chang = false; // 상근이가 가져간 상황 표현
 		}
 		else {
 			sang = false;
-			chang = true;
+			chang = true; // 창영이가 가져간 상황 표현
 		}
 		if (n == 0) {
 			if (sang == true)
@@ -28,7 +28,7 @@ int main()
 				cout << "CY" << endl;
 
 			return 0;
-		}
+		} // 돌이 0개가 되었을 때 true인 사람이 마지막으로 가져간 사람이므로, 상황에 맞게 
 	}
 
 }
